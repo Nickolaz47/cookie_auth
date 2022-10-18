dcd7eda898415cf258a17346660f6fb4e3be78b7
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-10-18 19:46:36 -0300

    Configured both bootstrap CSS and JS.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
