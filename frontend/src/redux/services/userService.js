83e5970a969d83053dc85d6cbef54fb677956a76
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-10-18 23:43:45 -0300

    Created register function in userSlice.

--- frontend/src/redux/services/userService.js
+++ frontend/src/redux/services/userService.js
@@ -1,8 +1,31 @@
 // Config
-import { usersUrl, requestConfig } from "../../utils/config";
+import { baseUrl, requestConfig } from "../../utils/config";
 // Requests
 import axios from "axios";
 
-const userService = {};
+const instance = axios.create({
+  baseURL: baseUrl,
+  withCredentials: true,
+  timeout: 5000,
+});
+
+const register = async (data) => {
+  const config = requestConfig("post", data);
+
+  try {
+    const res = await instance({ url: "/register", data }, config);
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+const login = async (req, res) => {};
+
+const logout = async (req, res) => {};
+
+const getUserData = async (req, res) => {};
+
+const userService = { register, login, logout, getUserData };
 
 export default userService;
--- frontend/src/redux/slices/userSlice.js
+++ frontend/src/redux/slices/userSlice.js
@@ -5,14 +5,51 @@ const initialState = {
   user: {},
   error: null,
   loading: false,
-  message: null,
+  success: false,
 };
+
+export const register = createAsyncThunk(
+  "user/register",
+  async (newUser, thunkAPI) => {
+    const data = await userService.register(newUser);
+
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {}
+  reducers: {
+    reset: (state) => {
+      state.loading = false;
+      state.error = null;
+      state.success = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.success = false;
+        state.user = null;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.success = true;
+        state.user = action.payload;
+      });
+  },
 });
 
 export default userSlice.reducer;
