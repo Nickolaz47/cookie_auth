49102eeba7176434d8e25f2e3e367394b27dd7d5
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-10-19 00:27:18 -0300

    Implemented redux store in index.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,16 @@ import App from './App';
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+// Redux
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
