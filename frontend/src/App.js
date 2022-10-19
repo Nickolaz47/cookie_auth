ed079251ffc49afe34332d141f39427d2e163238
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-10-19 14:26:07 -0300

    Created Dashboard page and control routes through authentication.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,38 @@
 // Pages
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
 // Components
 import NavMenu from "./components/NavMenu";
+// Hooks
+import { useAuth } from "./hooks/useAuth";
 // React Router
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 // CSS
 import "./App.css";
 
 function App() {
+  const { auth } = useAuth();
+  
   return (
     <div>
       <BrowserRouter>
         <NavMenu />
         <div className="set_page_size">
           <Routes>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/register" element={<Register />}></Route>
+            <Route path="/" element={auth ? <Dashboard /> : <Login />}></Route>
+            <Route
+              path="/dashboard"
+              element={auth ? <Dashboard /> : <Login />}
+            ></Route>
+            <Route
+              path="/login"
+              element={!auth ? <Login /> : <Dashboard />}
+            ></Route>
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Dashboard />}
+            ></Route>
           </Routes>
         </div>
       </BrowserRouter>
