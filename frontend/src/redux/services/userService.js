d6690f17be6b8a3f16c8972c386d40fa08f07ca9
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-11-07 11:05:01 -0300

    Minor changes in userService.

--- frontend/src/redux/services/userService.js
+++ frontend/src/redux/services/userService.js
@@ -11,14 +11,13 @@ axios.interceptors.response.use(
   async (error) => {
     const { response } = error;
     if (response.data.errors[0] === "Access token expirado!") {
-      localStorage.setItem("originalReq", JSON.stringify(response.config));
       const config = requestConfig(`${baseUrl}/token/refresh`, "GET");
-      const res = await axios(config);
-      return res;
+      await axios(config);
+      return;
     } else if (response.data.errors[0] === "Refresh token expirado!") {
       Cookies.remove("frontAuthCookie");
-      const res = await authService.logout();
-      return res;
+      await authService.logout();
+      return
     }
     return response;
   }
